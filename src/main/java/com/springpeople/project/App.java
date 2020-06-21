4cb533785b93ac592ea6e1464646592803e3a4fd
chandan.mishra <chandan.mishra@evolutionfinance.com>
2020-06-22 02:23:49 +0530

    first commit for jenkins-github webhook

---
+++ src/main/java/com/springpeople/project/App.java
@@ -0,0 +1,13 @@
+package com.springpeople.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!, Jenkins-bitbucket integration" );
+    }
+}
---
+++ src/test/java/com/springpeople/project/AppTest.java
@@ -0,0 +1,24 @@
+package com.springpeople.project;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	@Test
+	public void testcase1() {
+
+		assertEquals(true, true);
+	}
+
+	@Test
+	public void testcase2() {
+
+		assertEquals(true, true);
+	}
+
+}
