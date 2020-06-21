1ce7254f26b9609665aff9569d7c5321bfbc55c5
chandan.mishra <chandan.mishra@evolutionfinance.com>
2020-06-22 03:27:24 +0530

    first commit for jenkins-github webhook

--- src/main/java/com/springpeople/project/App.java
+++ src/main/java/com/springpeople/project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!, Jenkins-bitbucket1 integration" );
+        System.out.println( "Hello World!, Jenkins-github integration" );
     }
 }
